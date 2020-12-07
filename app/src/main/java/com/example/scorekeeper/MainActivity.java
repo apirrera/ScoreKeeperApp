6e0a5106b40ca3836f93ff2a792c96e3a87f2a6b
apirrera <pirreraantonella@gmail.com>
2020-12-06 21:36:28 -0500

    Version 1.0 (Final Design, Addition Function, and Subtraction Function)

--- app/src/main/java/com/example/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.scorekeeper;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+    private View add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void addOneForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void subOneForTeamA(View v) {
+        scoreTeamA = scoreTeamA - 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addOneForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void subOneForTeamB(View v) {
+        scoreTeamB = scoreTeamB - 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.scoreTeamA);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.scoreTeamB);
+        scoreView.setText(String.valueOf(score));
+    }
 }
+
